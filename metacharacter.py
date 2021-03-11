cabe9ee84ec018e45deabcc1428238ab03c2347b
99003720 <78869160+99003720@users.noreply.github.com>
2021-03-11 17:00:48 +0530

    commit file from pycharm on windows

--- metacharacter.py
+++ metacharacter.py
@@ -8,3 +8,14 @@ if re.match(pat, "gray"):
     print("M2")
 if re.match(pat, "stingray"):
     print("M3")
+
+pat = r"ice(-)?cream"
+
+if re.match(pat, "ice-cream"):
+    print("M1")
+if re.match(pat, "icecream"):
+    print("M2")
+if re.match(pat, "ice--ice"):
+    print("M3")
+if re.match(pat, "sausages"):
+    print("M3")
