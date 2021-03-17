efae0bb6b664f385ab1ac0d16230bf3bf14af079
99003720 <78869160+99003720@users.noreply.github.com>
2021-03-17 10:40:47 +0530

    commit file on windows

---
+++ Classes.py
@@ -0,0 +1,19 @@
+'''class car:
+    def __init__(self,modelname,year):
+        self.model = modelname
+        self.year = year
+    def display(self):
+        print(self.model,self.year)
+
+c1 = car("Toyota",2016)
+c1.display()
+'''
+class Employee:    
+    id = 10   
+    name = "Devansh"    
+    def display (self):    
+        print("ID: %d \nName: %s"%(self.id,self.name))    
+
+emp = Employee()    
+emp.display()  
+
